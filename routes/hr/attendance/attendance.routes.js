43b67079a6a4cc567873bb60372e085055331745
Muhammad Waheed Niaz <71188583+waheed7866@users.noreply.github.com>
2023-08-29 23:10:18 +0500

    ok

--- routes/hr/attendance/attendance.routes.js
+++ routes/hr/attendance/attendance.routes.js
@@ -6,8 +6,6 @@ const {
   getSingleAttendance,
   getAttendanceByUserId,
   getLastAttendanceByUserId,
-  search,
-  updateSingleAttendence,
 } = require("./attendance.controller");
 const authorize = require("../../../utils/authorize"); // authentication middleware
 
@@ -15,14 +13,8 @@ const attendanceRoutes = express.Router();
 
 attendanceRoutes.post("/", authorize(""), createAttendance);
 attendanceRoutes.get("/", authorize(""), getAllAttendance);
-attendanceRoutes.get("/", authorize(""), search);
 attendanceRoutes.get("/:id", authorize(""), getSingleAttendance);
 attendanceRoutes.get("/:id/user", authorize(""), getAttendanceByUserId);
 attendanceRoutes.get("/:id/last", authorize(""), getLastAttendanceByUserId);
-attendanceRoutes.put(
-  "/:id",
-  authorize("update-attendence"),
-  updateSingleAttendence
-);
 
 module.exports = attendanceRoutes;
