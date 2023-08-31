8d444fe69e9d492df4656c9c82d995d13351ac47
Muhammad Waheed Niaz <71188583+waheed7866@users.noreply.github.com>
2023-08-31 15:33:50 +0500

    ok

--- routes/hr/attendance/attendance.controller.js
+++ routes/hr/attendance/attendance.controller.js
@@ -110,7 +110,7 @@ const createAttendance = async (req, res) => {
       });
     } else  {
       const outTime = new Date(moment.now());
-      const totalHours = Math.abs(outTime - attendance.inTime) / 36e5;
+      const totalHours = Math.abs(outTime - attendance.inTime) /36000;
 
       const newAttendance = await prisma.attendance.update({
         where: {
@@ -132,12 +132,9 @@ const createAttendance = async (req, res) => {
   }
 };
 
-
 const createadminAttendance = async (req, res) => {
   try {
     const id = parseInt(req.body.userId);
-    const attendenceStatus = req.body.attendenceStatus || "Present";
-    const date = req.body.date || new Date();
     if (
       !(id === req.auth.sub) &&
       !req.auth.permissions.includes("create-attendance")
@@ -159,38 +156,32 @@ const createadminAttendance = async (req, res) => {
     // format time
     const startTime = moment(user.shift.startTime, "h:mm A");
     const endTime = moment(user.shift.endTime, "h:mm A");
+
+    // check if user is late or early
+    const isLate = moment().isAfter(startTime);
+    const isEarly = moment().isBefore(startTime);
+    const isOutEarly = moment().isAfter(endTime);
+    const isOutLate = moment().isBefore(endTime);
     const today = moment().startOf('day');
     const tomorrow = moment(today).add(1, 'days');
 
     const attendance = await prisma.attendance.findFirst({
       where: {
         userId: id,
-        inTime: {
-          gte: today.toDate(),
-          lt: tomorrow.toDate(),
-        },
       },
     });
     
   const existingCheckOut = await prisma.attendance.findFirst({
     where: {
       userId: id,
-      outTime: {
-        gte: today.toDate(),
-        lt: tomorrow.toDate(),
-      },
+     
     },
   });
-
-    if (attendance && existingCheckOut) {
-      return res.status(400).json({
-        message: "Clock in has already been marked for today.",
-      });
-    }
-
     if (req.query.query === "manualPunch") {
       const inTime = null;
+      const date = new Date();
       const outTime = null;
+
       const totalHours = null;
 
       const newAttendance = await prisma.attendance.create({
@@ -198,62 +189,58 @@ const createadminAttendance = async (req, res) => {
           userId: id,
           inTime: inTime,
           outTime: outTime,
-          attendenceStatus: attendenceStatus,
-          date: date,
-          inTimeStatus: null,
-          outTimeStatus: null,
-          comment: null,
-          ip: null,
-          totalHour: totalHours,
+          punchBy: req.auth.sub,
+          inTimeStatus: req.body.inTimeStatus ? req.body.inTimeStatus : null,
+          outTimeStatus: req.body.outTimeStatus ? req.body.outTimeStatus : null,
+          comment: req.body.comment ? req.body.comment : null,
+          date: req.body.date ? req.body.date : new Date(),
+          attendenceStatus:req.body.attendenceStatus ? req.body.attendenceStatus:"Present",
+          ip: req.body.ip ? req.body.ip : null,
+          totalHour: parseFloat(totalHours.toFixed(3)),
         },
       });
-
-      return res.status(201).json({
+      return res.status(200).json({
         newAttendance,
-        message: "Clock in Successfully"
+        message:"Admin mark attendence Successfully"
       });
     } else if (attendance === null) {
-      const inTime = null;
-      const outTime = null;
-      const totalHours = null;
-
+      const inTime = new Date("0000-01-01T00:00:00Z");
+      const outTime=new Date("0000-01-01T00:00:00Z")
       const newAttendance = await prisma.attendance.create({
         data: {
           userId: id,
           inTime: inTime,
           outTime: outTime,
-          attendenceStatus: attendenceStatus,
-          date: date,
+          punchBy: req.auth.sub,
+          comment: req.body.comment ? req.body.comment : null,
+          ip: req.body.ip ? req.body.ip : null,
+          date: req.body.date ? req.body.date :new Date(),
+          attendenceStatus: req.body.attendenceStatus ? req.body.attendenceStatus:"Present",
           inTimeStatus: null,
           outTimeStatus: null,
-          comment: null,
-          ip: null,
-          totalHour: totalHours,
         },
       });
-
-      return res.status(201).json({
+      return res.status(200).json({
         newAttendance,
-        message: "Clock in Successfully"
+        message:"Admin mark attendence Successfully"
       });
-    } else {
-      const outTime = new Date(moment.now());
-      const totalHours = Math.abs(outTime - attendance.inTime) / 36e5;
+    } else  {
+      const outTime = new Date("0000-01-01T00:00:00Z");
+      const totalHours =0;
 
-      const updatedAttendance = await prisma.attendance.update({
+      const newAttendance = await prisma.attendance.update({
         where: {
           id: attendance.id,
         },
         data: {
           outTime: outTime,
-          totalHour: parseFloat(totalHours.toFixed(3)),
-          outTimeStatus: null,
+          totalHour: null,
+          outTimeStatus:null,
         },
       });
-
       return res.status(200).json({
-        updatedAttendance,
-        message: "Clock out Successfully"
+        newAttendance,
+        message:"Admin mark attendence Successfully"
       });
     }
   } catch (error) {
@@ -261,146 +248,6 @@ const createadminAttendance = async (req, res) => {
   }
 };
 
-
-
-
-
-
-
-// const createAttendance = async (req, res) => {
-//   try {
-//     const id = parseInt(req.body.userId);
-//     if (
-//       !(id === req.auth.sub) &&
-//       !req.auth.permissions.includes("create-attendance")
-//     ) {
-//       return res.status(401).json({
-//         message: "Unauthorized. You are not authorized to give attendance",
-//       });
-//     }
-//     // get user shift
-//     const user = await prisma.user.findUnique({
-//       where: {
-//         id: id,
-//       },
-//       include: {
-//         shift: true,
-//       },
-//     });
-
-//     // format time
-//     const startTime = moment(user.shift.startTime, "h:mm A");
-//     const endTime = moment(user.shift.endTime, "h:mm A");
-
-//     // check if user is late or early
-//     const isLate = moment().isAfter(startTime);
-//     const isEarly = moment().isBefore(startTime);
-//     const isOutEarly = moment().isAfter(endTime);
-//     const isOutLate = moment().isBefore(endTime);
-
-//     const attendance = await prisma.attendance.findFirst({
-//       where: {
-//         userId: id,
-//         outTime: null,
-//       },
-//     });
-
-//     // Get the user's location data from the request
-//     const userLatitude = parseFloat(req.body.latitude);
-//     const userLongitude = parseFloat(req.body.longitude);
-
-//     // Get the stored location from the database
-//     const storedLocation = await prisma.location.findFirst();
-
-//     if (!storedLocation) {
-//       return res.status(404).json({ message: "Location not found." });
-//     }
-
-//     const storedLatitude = parseFloat(storedLocation.latitude);
-//     const storedLongitude = parseFloat(storedLocation.longitude);
-
-//     // Calculate the distance between user's location and stored location
-//     const distanceInMeters = calculateDistance(userLatitude, userLongitude, storedLatitude, storedLongitude);
-
-//     // Set the threshold for attendance creation (e.g., 100 meters)
-//     const attendanceThreshold = 100; // You can adjust this value as needed
-
-//     if (distanceInMeters <= attendanceThreshold) {
-//       if (req.query.query === "manualPunch") {
-//         const inTime = new Date(req.body.inTime);
-//         const outTime = new Date(req.body.outTime);
-  
-//         const totalHours = Math.abs(outTime - inTime) / 36e5;
-  
-//         const newAttendance = await prisma.attendance.create({
-//           data: {
-//             userId: id,
-//             inTime: inTime,
-//             outTime: outTime,
-//             punchBy: req.auth.sub,
-//             inTimeStatus: req.body.inTimeStatus ? req.body.inTimeStatus : null,
-//             outTimeStatus: req.body.outTimeStatus ? req.body.outTimeStatus : null,
-//             comment: req.body.comment ? req.body.comment : null,
-//             ip: req.body.ip ? req.body.ip : null,
-//             totalHour: parseFloat(totalHours.toFixed(3)),
-//           },
-//         });
-//         return res.status(201).json(newAttendance);
-//       } else if (attendance === null) {
-//         const inTime = new Date(moment.now());
-//         const newAttendance = await prisma.attendance.create({
-//           data: {
-//             userId: id,
-//             inTime: inTime,
-//             outTime: null,
-//             punchBy: req.auth.sub,
-//             comment: req.body.comment ? req.body.comment : null,
-//             ip: req.body.ip ? req.body.ip : null,
-//             inTimeStatus: isEarly ? "Early" : isLate ? "Late" : "On Time",
-//             outTimeStatus: null,
-//           },
-//         });
-//         return res.status(201).json(newAttendance);
-//       } else {
-//         const outTime = new Date(moment.now());
-//         const totalHours = Math.abs(outTime - attendance.inTime) / 36e5;
-  
-//         const newAttendance = await prisma.attendance.update({
-//           where: {
-//             id: attendance.id,
-//           },
-//           data: {
-//             outTime: outTime,
-//             totalHour: parseFloat(totalHours.toFixed(3)),
-//             outTimeStatus: isOutEarly ? "Early" : isOutLate ? "Late" : "On Time",
-//           },
-//         });
-//         return res.status(200).json(newAttendance);
-//       }
-//     } else {
-//       return res.status(400).json({ message: "User is not within attendance range." });
-//     }
-//   } catch (error) {
-//     return res.status(400).json({ message: error.message });
-//   }
-// };
-
-// // // Function to calculate distance between two points using Haversine formula
-// function calculateDistance(lat1, lon1, lat2, lon2) {
-//   const R = 6371e3; // Earth's radius in meters
-//   const phi1 = lat1 * (Math.PI / 180);
-//   const phi2 = lat2 * (Math.PI / 180);
-//   const deltaPhi = (lat2 - lat1) * (Math.PI / 180);
-//   const deltaLambda = (lon2 - lon1) * (Math.PI / 180);
-
-//   const a = Math.sin(deltaPhi / 2) * Math.sin(deltaPhi / 2) +
-//             Math.cos(phi1) * Math.cos(phi2) *
-//             Math.sin(deltaLambda / 2) * Math.sin(deltaLambda / 2);
-//   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-//   const distance = R * c;
-//   return distance;
-// }
 
 const getAllAttendance = async (req, res) => {
   if (!req.auth.permissions.includes("readAll-attendance")) {
