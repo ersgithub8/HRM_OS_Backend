2227f00a16e0b5b9f6a2df31bd522b6b93e8df3e
Muhammad Waheed Niaz <71188583+waheed7866@users.noreply.github.com>
2023-09-04 11:54:21 +0500

    ok

--- routes/user/user.controller.js
+++ routes/user/user.controller.js
@@ -268,7 +268,8 @@ const getAllUser = async (req, res) => {
             const { password, ...userWithoutPassword } = u;
             return userWithoutPassword;
           })
-          .sort((a, b) => a.id - b.id)
+          .sort((a, b) => b.id - a.id)
+
       );
     } catch (error) {
       return res.status(500).json({ message: error.message });
