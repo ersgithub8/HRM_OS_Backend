7df3e775a17f9c81f1214ee8a1835810abe5fcf9
Muhammad Waheed Niaz <71188583+waheed7866@users.noreply.github.com>
2023-09-06 16:09:04 +0500

    ok

--- routes/hr/attendance/attendance.controller.js
+++ routes/hr/attendance/attendance.controller.js
@@ -105,7 +105,7 @@ const createAttendance = async (req, res) => {
           ip: req.body.ip ? req.body.ip : null,
           date: req.body.date ? req.body.date :new Date(),
           attendenceStatus: req.body.attendenceStatus ? req.body.attendenceStatus:"present",
-          inTimeStatus: isEarly ? "Early" : isLate ? "Late" : "On Time",
+          inTimeStatus: isEarly ? "OnTime" : isLate ? "Late" : "OnTime",
           outTimeStatus: null,
         },
       });
@@ -124,7 +124,7 @@ const createAttendance = async (req, res) => {
         data: {
           outTime: outTime,
           totalHour: parseFloat(totalHours.toFixed(3)),
-          outTimeStatus: isOutEarly ? "Early" : isOutLate ? "Late" : "On Time",
+          outTimeStatus: isOutEarly ? "Early" : isOutLate ? "OnTime" : "OnTime",
         },
       });
       return res.status(200).json({
