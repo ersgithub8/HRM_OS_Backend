f5693b422c113bdfae2daaa2653f444797cbe011
Muhammad Waheed Niaz <71188583+waheed7866@users.noreply.github.com>
2023-09-15 00:44:53 +0500

    ok

--- routes/user/user.controller.js
+++ routes/user/user.controller.js
@@ -642,7 +642,7 @@ const updateSingleUser = async (req, res) => {
       country: req.body.country,
       departmentId: req.body.departmentId,
       roleId: req.body.roleId,
-      reference_id:req.body.id,
+      reference_id:id,
       shiftId: req.body.shiftId,
       locationId: req.body.locationId,
       leavePolicyId: req.body.leavePolicyId,
