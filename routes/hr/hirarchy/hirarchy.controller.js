1132ee223dfe7b91c8345087d9a9f58c751c1cf6
Muhammad Waheed Niaz <71188583+waheed7866@users.noreply.github.com>
2023-09-28 17:10:12 +0500

    ok

--- routes/hr/hirarchy/hirarchy.controller.js
+++ routes/hr/hirarchy/hirarchy.controller.js
@@ -57,6 +57,60 @@ const prisma = require("../../../utils/prisma");
   }
 
 
+  const rolenew=async(req,res)=>{
+    let rollId=Number(req.params.id);
+    let singleRole;
+    let reqq=null;
+    if(rollId==3){
+        singleRole= null;
+    }
+    else if(rollId==4){
+        reqq =4;
+    }
+    else if(rollId==5){
+        reqq=5;
+    }
+    else if(rollId==6){
+        reqq=6;
+    }
+    else if(rollId==1){
+        reqq=1;
+    }
+
+    else if(rollId==7){
+        reqq=7;
+    }
+    else if(rollId==8){
+        reqq=8;
+    }
+    else if(rollId==2){
+        reqq=2;
+    }
+
+
+    if(reqq!=null){
+        singleRole = await prisma.user.findMany({
+            where: {
+              roleId: reqq,
+            }
+            
+          });
+    }
+    // const roleId = singleRole.reference_id; // Assuming reference_id is the field where the roll id is saved
+    // const superviser = await prisma.role.findMany({
+    //   where: {
+    //     id: roleId,
+    //   },
+    // });
+
+    // // Add the roleType to the user data
+    // singleRole.superviser = superviser;
+     res.status(200).json({
+      singleRole
+     })
+  }
+
+
 
 
 
@@ -68,4 +122,5 @@ const prisma = require("../../../utils/prisma");
 
 module.exports = {
     role,
+    rolenew
   };
--- routes/hr/hirarchy/hirarchy.route.js
+++ routes/hr/hirarchy/hirarchy.route.js
@@ -2,8 +2,10 @@ const express = require("express");
 const hirarchyRoutes = express.Router();
 
 const {
-    role
+    role,
+    rolenew
 } = require("./hirarchy.controller");
 const authorize = require("../../../utils/authorize"); 
-hirarchyRoutes.get("/:id", authorize("readSingle-user"), role); 
+hirarchyRoutes.get("/:id", authorize("readSingle-user"), role);
+hirarchyRoutes.get("/rolenew/:id", authorize("readSingle-user"), rolenew); 
 module.exports = hirarchyRoutes;
