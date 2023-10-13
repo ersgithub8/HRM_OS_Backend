86dd360bd715c40d0feaeb8aea38898121308fba
Haider Raza <88819339+SyedHaadi@users.noreply.github.com>
2023-10-13 14:48:12 +0500

    ok

--- routes/user/user.controller.js
+++ routes/user/user.controller.js
@@ -187,7 +187,7 @@ const register = async (req, res) => {
   
   if (req.body.manualleave) {
     remainingannualallowedleave = req.body.manualleave.toString();
-  } else if (!req.body.manualleave) {
+  } else if (!req.body.leavePolicyId) {
     remainingannualallowedleave = process.env.remainingannualallowedleave;
   } else {
     remainingannualallowedleave = leavs ? leavs.paidLeaveCount.toString() : null;
