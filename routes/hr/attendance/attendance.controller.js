d18e3fe8feb95660e98295252825822961de16d3
Haider Raza <88819339+SyedHaadi@users.noreply.github.com>
2023-10-18 11:31:02 +0500

    ok

--- routes/hr/attendance/attendance.controller.js
+++ routes/hr/attendance/attendance.controller.js
@@ -804,7 +804,7 @@ const getTodayAttendanceByUserId = async (req, res) => {
     // // return
     // today.setHours(0, 0, 0, 0);
     const today = new Date();  
-    const databaseRecordDate = new Date("2023-10-18T11:05:01.890Z");
+    const databaseRecordDate = new Date("2023-10-19T06:27:21.582Z");
     today.setHours(databaseRecordDate.getHours(), databaseRecordDate.getMinutes(), databaseRecordDate.getSeconds(), databaseRecordDate.getMilliseconds());
     
     console.log(today);
