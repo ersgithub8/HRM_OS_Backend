288e1fb1999cad1ada33fcdf8e34e4a062d372a7
Haider Raza <88819339+SyedHaadi@users.noreply.github.com>
2023-10-24 17:58:03 +0500

    ok

--- routes/shifts/shifts.routes.js
+++ routes/shifts/shifts.routes.js
@@ -5,7 +5,6 @@ const {
   getSingleShift,
   updateSingleShift,
   deleteSingleShift,
-  getSingleShiftbyuserId
 } = require("./shifts.controller");
 const authorize = require("../../utils/authorize"); // authentication middleware
 
@@ -14,7 +13,6 @@ const shiftsRoutes = express.Router();
 shiftsRoutes.post("/", authorize("create-shift"), createShift);
 shiftsRoutes.get("/", authorize("readAll-shift"), getAllShift);
 shiftsRoutes.get("/:id", authorize("readSingle-shift"), getSingleShift);
-shiftsRoutes.get("/user/:id", authorize("readSingle-shift"), getSingleShiftbyuserId);
 shiftsRoutes.put("/update/:id", authorize("update-shift"), updateSingleShift);
 shiftsRoutes.delete("/:id", authorize("delete-shift"), deleteSingleShift);
 module.exports = shiftsRoutes;
